9db6ebb29255ae1a960f9af1c2f93f80d042a89d
devtoguk <devtoguk@gmail.com>
2020-05-18 20:47:12 +0000

    Change calc to object

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,7 +1,23 @@
-function addition(numOne, numTwo) {
+Calculator = function() {
+    this.value = 0;
+}
+
+Calculator.prototype.add = function(number) {
     if(typeof(numOne) == "number" && typeof(numTwo) == "number") {
-        return numOne + numTwo;
+        this.value += number;
     } else {
         alert("Error!");
     }
 }
+
+
+
+
+
+// function addition(numOne, numTwo) {
+//     if(typeof(numOne) == "number" && typeof(numTwo) == "number") {
+//         return numOne + numTwo;
+//     } else {
+//         alert("Error!");
+//     }
+// }
